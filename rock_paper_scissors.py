924387ee155e4676f62696bf247e396174873c59
doughbryce <31143323+doughbryce@users.noreply.github.com>
2022-07-01 15:23:31 -0600

    1

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,14 +1,18 @@
-computer_choice = 'scissors'
+import random
 
-user_choice = input("Do you want - rock, paper, or scissors?\n")
+choices_list = ['rock', 'paper', 'scissors']
 
-if computer_choice == user_choice:
-    print('TIE')
-elif user_choice == 'rock' and computer_choice == 'scissors':
-    print('WIN')
-elif user_choice == 'paper' and computer_choice == 'rock':
-    print('WIN')
-elif user_choice == 'scissors' and computer_choice == 'paper':
-    print('WIN')
-else:
-    print('You lose :( Computer wins :D')
+computer_choice = random.choice(choices_list)
+
+user_choice = input("do you want rock, paper, or scissors?\n")
+
+if (computer_choice == user_choice):
+    print ("TIE")
+elif (user_choice == "rock" and computer_choice == "scissors"):
+    print ("WIN")
+elif (user_choice == "paper" and computer_choice == "rock"):
+    print ("WIN")
+elif (user_choice == "scissors" and computer_choice == "paper"):
+    print ("WIN")
+else: 
+    print ("LOSE")
